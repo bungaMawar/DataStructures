5879564aa11b2418db3d20dc04ecd59b37de8c7b
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2024-05-18 11:36:41 -0400

    alternate counting sort

--- CountingSort.java
+++ CountingSort.java
@@ -1,36 +1,24 @@
 public class CountingSort {
-    public static void main(String args[]){
-        int array[] = {1, 2, 10, 1, 4, 5, 4, 7, 4, 9};
-        int[] countingArray = new int[array.length];
+    private static void countingSort(int [] input, int min, int max){
+        int[] countArray = new int[(max - min) + 1];
 
-        for(int i = 0; i < countingArray.length; i++){
-            countingArray[i] = 0;
+        for(int i = 0; i < input.length; i++){
+            countArray[input[i] - min]++;
         }
 
-        for(int i = 0; i < array.length; i++){
-            countingArray[array[i] - 1]++;
-        }
-
-        System.out.print("counting array: ");
-        for(int j = 0; j < countingArray.length; j++){
-            System.out.print(countingArray[j] + " ");
-        }
-        System.out.println();    
-        
-        int arrayIndex = 0;
-        for(int i = 0; i < countingArray.length; i++){
-            if(countingArray[i] > 0){
-                int temp = countingArray[i];
-                while(temp != 0){
-                    array[arrayIndex] = i + 1;
-                    arrayIndex++;
-                    temp--;
-                }
+        int j = 0; 
+        for(int i =  min; i <= max; i++){
+            while(countArray[i - min] > 0){
+                input[j++] = i;
+                countArray[i - min]--;
             }
         }
-
+    }
+    public static void main(String args[]){
+        int array[] = {1, 2, 10, 1, 4, 5, 4, 7, 4, 9};
+        countingSort(array, 1, 10);
         System.out.print("array: ");
-        for(int j = 0; j < countingArray.length; j++){
+        for(int j = 0; j < array.length; j++){
             System.out.print(array[j] + " ");
         }
         System.out.println(); 
