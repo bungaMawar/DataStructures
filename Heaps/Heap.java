b89c3941a538084b909ee497db126874c381de82
Shafiya Salahuddin <shafiya.salahuddin@gmail.com>
2024-07-22 10:35:16 -0400

    Heaps delete

--- Heaps/Heap.java
+++ Heaps/Heap.java
@@ -1,3 +1,5 @@
+package Heaps;
+
 public class Heap {
     private int[] heap;
     private int size;
@@ -19,7 +21,7 @@ public class Heap {
             throw new IndexOutOfBoundsException("Heap is full");
         }
 
-        heap[size++] = value;
+        heap[size] = value;
 
         fixHeapAbove(size);
         size++;
@@ -32,5 +34,67 @@ public class Heap {
             index = getParent(index);
         }
         heap[index] = newValue;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int getChild(int index, boolean left){
+        return 2 * index + (left ? 1 : 2); 
+    }
+
+    public int delete(int index){
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        if(index == 0 || heap[index] < heap[parent]){
+            fixHeapBelow(index, index);
+        } else{
+            fixHeapAbove(index);
+        }
+        size--;
+        return deletedValue;
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex){
+        int childToSwap;
+
+        while(index <= lastHeapIndex){
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+
+            if(leftChild <= lastHeapIndex){
+                if(rightChild > lastHeapIndex){
+                    childToSwap = leftChild;
+                } else{
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+
+                if(heap[index] < heap[childToSwap]){
+                    int temp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = temp;
+                } else{
+                    break;
+                }
+                index = childToSwap;
+            } else{
+                break;
+            }
+        }
+    }
+
+    public void printHeap(){
+        for(int i = 0; i < size; i++){
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
     }
 }
